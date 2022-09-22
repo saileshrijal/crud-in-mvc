070d36b3c145ec51c1bd91971e38abcb0244ab4a
Abhishek Bohora <abhishek.bohora16@gmail.com>
2022-09-22 20:57:08 +0545

    closes #1; added StudentDetailsModel and performed migration

---
+++ crud/Data/StudentsDetailDbContext.cs
@@ -0,0 +1,13 @@
+﻿using crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.Data
+{
+    public class StudentsDetailDbContext : DbContext
+    {
+        public StudentsDetailDbContext(DbContextOptions options): base(options){
+        
+        }
+        public DbSet<StudentDetails> StudentsDetails { get; set; } 
+    }
+}
